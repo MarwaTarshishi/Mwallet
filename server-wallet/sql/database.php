b73e882e302cffe8cc3fbb8c67eae5eeecdf8de1
Marwa Tarshishi <marwatarshishi201@gmail.com>
2025-03-04 12:34:48 +0200

    Update database.php

--- server-wallet/sql/database.php
+++ server-wallet/sql/database.php
@@ -1,1 +1,24 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "vanilla_wallet";
+    private $username = "root";    // Your MySQL username (default is "root")
+    private $password = "";        // Your MySQL password (default is empty "")
+    private $conn;
 
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
