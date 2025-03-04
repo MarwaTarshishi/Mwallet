5e0c8301616d3808222d5f8e8eda7b8a05dd27ce
Marwa Tarshishi <marwatarshishi201@gmail.com>
2025-03-04 12:54:40 +0200

    Add object properties

--- server-wallet/models/Notification.php
+++ server-wallet/models/Notification.php
@@ -1,1 +1,16 @@
-
+<?php
+class Notification {
+    private $conn;
+    private $table_name = "notifications";
+    public $notification_id;
+    public $user_id;
+    public $title;
+    public $message;
+    public $is_read;
+    public $created_at;
+    
+    // Constructor
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+    
