3f2f6bd86cb6e49588e1ee941307e05031d42e8c
Marwa Tarshishi <marwatarshishi201@gmail.com>
2025-03-04 12:55:14 +0200

    Create query to add notification

--- server-wallet/models/Notification.php
+++ server-wallet/models/Notification.php
@@ -13,4 +13,28 @@ class Notification {
     public function __construct($db) {
         $this->conn = $db;
     }
-    
+    public function create() {
+        // Query to insert a new notification
+        $query = "INSERT INTO " . $this->table_name . " (user_id, title, message) VALUES (?, ?, ?)";
+        
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
+        
+        // Sanitize and bind parameters
+        $this->user_id = htmlspecialchars(strip_tags($this->user_id));
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->message = htmlspecialchars(strip_tags($this->message));
+        
+        $stmt->bindParam(1, $this->user_id);
+        $stmt->bindParam(2, $this->title);
+        $stmt->bindParam(3, $this->message);
+        
+        // Execute query
+        if ($stmt->execute()) {
+            // Get the newly created notification ID
+            $this->notification_id = $this->conn->lastInsertId();
+            return true;
+        }
+        
+        return false;
+    }
