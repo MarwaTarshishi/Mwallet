4c72e2ecd41e834d2e90e32f11f68ed409cf6b00
Marwa Tarshishi <marwatarshishi201@gmail.com>
2025-03-04 23:32:51 +0200

    Update transaction.routes.js

--- server-wallet/routes/transaction.routes.js
+++ server-wallet/routes/transaction.routes.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const TransactionController = require('../controllers/transaction.controller');
+
+router.use(auth); // Protect all transaction routes
+
+router.get('/', TransactionController.getAllTransactions);
+router.get('/summary', TransactionController.getTransactionSummary);
+router.get('/:id', TransactionController.getTransactionById);
+router.post('/', TransactionController.createTransaction);
+
+module.exports = router;
 
