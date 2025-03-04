257181ced6f7bb0b16bb3caa7572ea4b6595ff8a
Marwa Tarshishi <marwatarshishi201@gmail.com>
2025-03-04 23:42:49 +0200

    Update transaction.service.js

--- server-wallet/src/services/transaction.service.js
+++ server-wallet/src/services/transaction.service.js
@@ -1,1 +1,33 @@
+import api from './api';
+
+class TransactionService {
+    async getAllTransactions(filters = {}) {
+        try {
+            const response = await api.get('/transaction', { params: filters });
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async createTransaction(transactionData) {
+        try {
+            const response = await api.post('/transaction', transactionData);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getWalletBalance() {
+        try {
+            const response = await api.get('/wallet/balance');
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export default new TransactionService();
 
