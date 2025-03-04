0c02d9813aca8c62a5bf991c8f7eb9fc9d5aa4e1
Marwa Tarshishi <marwatarshishi201@gmail.com>
2025-03-04 02:28:11 +0200

    Import api from './api'

--- user-wallet/service/auth.service.js
+++ user-wallet/service/auth.service.js
@@ -1,1 +1,35 @@
+import api from './api';
 
+class AuthService {
+    async login(email, password) {
+        try {
+            const response = await api.post('/user/login', { email, password });
+            if (response.data.token) {
+                localStorage.setItem('token', response.data.token);
+                localStorage.setItem('user', JSON.stringify(response.data.user));
+            }
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async register(userData) {
+        return api.post('/user/register', userData);
+    }
+
+    logout() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+    }
+
+    getCurrentUser() {
+        return JSON.parse(localStorage.getItem('user'));
+    }
+
+    isAuthenticated() {
+        return !!localStorage.getItem('token');
+    }
+}
+
+export default new AuthService();
