a0bb9aaa5bf2025a10d489205e83add90e14e5cb
Marwa Tarshishi <marwatarshishi201@gmail.com>
2025-03-04 11:48:08 +0200

    Include database and object files

--- server-wallet/api/get_balance.php
+++ server-wallet/api/get_balance.php
@@ -5,3 +5,6 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+include_once '../../config/database.php';
+include_once '../../models/Wallet.php';
+include_once '../../utils/jwt_auth.php';
